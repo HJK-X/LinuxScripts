e135f33243a60b20ff7b49ad1a234cf67e77c9e1
Justin <34551787+HJK-X@users.noreply.github.com>
2024-03-05 02:43:16 -0800

    Update basicsetup.sh

--- basicsetup.sh
+++ basicsetup.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
 
-# WiFi Connection
-
-# Prompt user for device
-iwctl device list
-read -p "Enter device: " iwctl_device
-iwctl device $iwctl_device set-property Powered on
-
-# Attempt to connect to WiFi
-while true; do
-    # Prompt the user for the SSID
-    iwctl station $iwctl_device scan
-    iwctl station $iwctl_device get-networks
-    read -p "Enter SSID: " ssid
-
-    iwctl station $iwctl_device connect "$ssid"
-    if [ $? -eq 0 ]; then
-        echo "Connected to $ssid successfully!"
-        break
-    else
-        echo "Failed to connect. Retrying..."
-        sleep 2
-    fi
-done
-
-# Disk Partition
-
-# Prompt user for disk
-fdisk -l
-read -p "Enter disk: " disk
-
-fdisk $disk 
-
 # Copy config files
 cp -av ./home/. ~
 cp -av ./etc/. /etc
