3e3a018891f3df5ecf2db7204cb6a06f10d1f2a1
Justin Kim <34551787+HJK-X@users.noreply.github.com>
2024-03-06 01:34:52 -0800

    gitconfig

--- basicsetup.sh
+++ basicsetup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo pacman -Sy i3 kitty dmenu tmux github-cli firefox
-
 # Copy config files
 cp -av ./home/. ~
 sudo cp -av ./etc/. /etc
 
 # Clear unnecessary files
 sudo rm -rf /etc/i3/
+
+
+gh auth login
