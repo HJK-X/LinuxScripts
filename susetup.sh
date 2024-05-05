9d8060ad37a90690602a0dcdc952e9ecda08001a
Justin Kim <34551787+HJK-X@users.noreply.github.com>
2024-05-04 21:51:31 -0700

    usr in

--- susetup.sh
+++ susetup.sh
@@ -8,6 +8,8 @@ sudo whoami
 chown -R root:root ./etc/.
 cp -av ./etc/. /etc/.
 cp -av ./cmd/. /usr/bin/.
+cp -av ./usr/. /usr/.
+
 
 # Start any system startup programs
 systemctl start NetworkManager.service
