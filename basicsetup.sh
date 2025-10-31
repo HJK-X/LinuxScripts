e21a67905373cae136e1cc611fdb3b3ac27fce8a
HJK-X <34551787+HJK-X@users.noreply.github.com>
2025-10-31 11:53:43 -0700

    something idk

--- basicsetup.sh
+++ basicsetup.sh
@@ -9,14 +9,18 @@ colors_conf="./info/colors.conf"
 
 # Read the colors.conf file line by line
 while IFS= read -r line; do
-    # Extract variable name and color code
-    var_name=$(echo "$line" | awk '{print $2}')
-    color=$(echo "$line" | awk '{print $3}')
+  # Extract variable name and color code
+  var_name=$(echo "$line" | awk '{print $2}')
+  color=$(echo "$line" | awk '{print $3}')
 
-    # Use find to locate all files in the folder and its subfolders
-    find ./tmp -type f -exec sed -i "s/$var_name/$color/g" {} +
-done < "$colors_conf"
+  # Use find to locate all files in the folder and its subfolders
+  find ./tmp -type f -exec sed -i "s/$var_name/$color/g" {} +
+done <"$colors_conf"
 
 # Copy folders to home dir
 cp -av ./tmp/. ~
 rm -r ./tmp
+
+for i in {0..9}; do
+  gsettings set org.gnome.shell.keybindings switch-to-application-$i "[]"
+done
